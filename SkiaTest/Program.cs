6f695f472363f6f3389c301ad231e37006275006
Gernot Lenkner <g.lenkner@outlook.de>
2019-01-30 16:37:27 +0100

    added a skia test project.

---
+++ SkiaTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using Cave.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SkiaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("no file specified to check.");
+                return;
+            }
+
+            string fileName = args[0];
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"file {fileName} not found!");
+                return;
+            }
+            Console.WriteLine("starting test, loading skia...");
+            Bitmap32.Loader = new SkiaBitmap32Loader();
+            Console.WriteLine("loading file...");
+            Bitmap32 b = Bitmap32.FromFile(fileName);
+            Console.WriteLine("file loaded!");
+            Console.WriteLine("Info:");
+            Console.WriteLine($"Bitmap size: {b.Width},{b.Height}");
+            Console.WriteLine("Detect max 4 Colors...");
+            IList<ARGB> colors = b.DetectColors(4);
+            foreach (var color in colors)
+            {
+                Console.WriteLine($"  {color}");
+            }
+            Console.WriteLine("finished.");
+        }
+    }
+}
