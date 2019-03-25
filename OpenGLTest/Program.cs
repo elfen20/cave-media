b68025f571ff78dfb9da01e76007b6fabce3210a
Gernot Lenkner <g.lenkner@outlook.de>
2019-03-25 13:51:05 +0100

    + OpenGLTest to testing.

---
+++ OpenGLTest/Program.cs
@@ -0,0 +1,233 @@
+﻿using Cave.Console;
+using Cave.Media;
+using Cave.Media.OpenGL;
+using Cave.Media.Video;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+
+namespace OpenGLTest
+{
+
+    public static class Extensions
+    {
+
+        public static T Next<T>(this T src) where T : struct
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argument {0} is not an Enum", typeof(T).FullName));
+
+            T[] Arr = (T[])Enum.GetValues(src.GetType());
+            int j = Array.IndexOf<T>(Arr, src) + 1;
+            return (Arr.Length == j) ? Arr[0] : Arr[j];
+        }
+    }
+
+    class Program
+    {
+        Glfw3Renderer renderer;
+        bool closed = false;
+        bool verbose = false;
+
+        int testNr = 0;
+        int width = 800;
+        int height = 400;
+        int display = 0;
+        RendererMode rmode = RendererMode.Window;
+
+
+        List<IRenderSprite> rSprites = new List<IRenderSprite>();
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Run(args);
+            SystemConsole.WriteLine("exit program.");
+        }
+
+        private void Run(string[] oldArgs)
+        {
+            var args = Arguments.FromArray(oldArgs);
+
+            if (args.IsHelpOptionFound())
+            {
+                SystemConsole.WriteLine("Options:");
+                SystemConsole.WriteLine("<yellow>-v<default> \t show verbose output");
+                SystemConsole.WriteLine("<yellow>-s<default> \t show available displays");
+                SystemConsole.WriteLine("<yellow>-d=<green><display><default> \t set display");
+                SystemConsole.WriteLine("<yellow>-b=<green><w>x<h><default> \t set backbuffer size in width x height");
+                SystemConsole.WriteLine("<yellow>-w, -f, -wf<green>[w][f][wf]<default> \t set window mode to window, fullscreen or windowed fullscreen");
+                SystemConsole.WriteLine("<yellow>-t=<green><n><default> \t use test number n");
+                return;
+            }
+
+            if (args.IsOptionPresent("v"))
+            {
+                verbose = true;
+                SystemConsole.WriteLine("<yellow>Verbose output activated!<default>");
+            }
+
+            if (args.IsOptionPresent("w"))
+            {
+                rmode = RendererMode.Window;
+                SystemConsole.WriteLine($"<yellow>Rendermode: <green>{rmode}<default>");
+            }
+            if (args.IsOptionPresent("f"))
+            {
+                rmode = RendererMode.FullScreen;
+                SystemConsole.WriteLine($"<yellow>Rendermode: <green>{rmode}<default>");
+            }
+            if (args.IsOptionPresent("wf"))
+            {
+                rmode = RendererMode.WindowedFullScreen;
+                SystemConsole.WriteLine($"<yellow>Rendermode: <green>{rmode}<default>");
+            }
+
+
+            if (args.IsOptionPresent("b"))
+            {
+                string[] dims = args.Options["b"].Value.Split('x');
+                if (dims.Length != 2) throw new Exception("backbuffer size has to be in form of (width)x(height) e.g. 800x600!");
+                if (!int.TryParse(dims[0], out width)) throw new Exception("error parsing backbuffer width!");
+                if (!int.TryParse(dims[1], out height)) throw new Exception("error parsing backbuffer height!");
+                SystemConsole.WriteLine($"<yellow>backbuffer set: {width} x {height}<default>");
+            }
+
+            Initialize(args);
+            if (args.IsOptionPresent("t"))
+            {
+                if (!int.TryParse(args.Options["t"].Value, out testNr)) throw new Exception("error parsing test number");
+                CreateSprites();
+            }
+            Play();
+        }
+
+        private void CreateSprites()
+        {
+            if (testNr > 0)
+            {
+                var bg = renderer.CreateSprite("1");
+                bg.LoadTexture(new GdiBitmap32(TestImageGenerator.GenerateCheckerBoard(new Size(64,64),new Size(8,8),Color.Blue, Color.Gray)));
+                rSprites.Add(bg);
+            }
+            if (testNr > 1)
+            {
+                var fps = renderer.CreateSprite("fps");
+               // fps.Tint = Color.Red;
+                fps.Position = Vector3.Create(-0.75f, -0.75f, 0f);
+                fps.Scale = Vector3.Create(0.3f, 0.2f, 1);
+                rSprites.Add(fps);
+
+            }
+            if (testNr > 2)
+            {
+                var rect = renderer.CreateSprite("rect");
+                rect.Alpha = 0.6f;
+                rect.Scale = Vector3.Create(0.6f, 0.6f, 1);
+                rect.LoadTexture(new GdiBitmap32(TestImageGenerator.GenerateColorStripes(new Size(256, 256))));
+                rSprites.Add(rect);
+
+            }
+        }
+
+        private void Play()
+        {
+            SystemConsole.WriteLine("start OpenGLTest:");
+            int counter = 0;
+            DateTime starttime = DateTime.Now;
+            DateTime runsince = starttime;
+            TimeSpan onesec = TimeSpan.FromSeconds(1);
+            while (!closed)
+            {
+                UpdateSprites(DateTime.Now - runsince);
+                Render();
+                counter++;
+                float msecs = (float)(DateTime.Now - runsince).TotalMilliseconds;
+                if ((DateTime.Now - starttime) > onesec)
+                {
+                    SystemConsole.WriteLine($"FPS: <yellow>{counter}<default>");
+                    counter = 0;
+                    starttime = DateTime.Now;
+                }
+                Thread.Sleep(1);
+                if (SystemConsole.KeyAvailable) closed = true;
+            }
+        }
+
+        private void UpdateSprites(TimeSpan timeSpan)
+        {
+           if (testNr > 1)
+            {
+                string ms = (timeSpan.TotalMilliseconds / 1000f).ToString("F2");
+                rSprites[1].LoadTexture(new GdiBitmap32(TestImageGenerator.GenerateString(new Size(150, 50), Color.Goldenrod, Color.Transparent, ms,0.8f)));
+            }
+            if (testNr > 1)
+            {
+                rSprites[2].Rotation = Vector3.Create(0,0,(float)timeSpan.TotalMilliseconds / 3000);
+            }
+        }
+
+        private void Render()
+        {
+            renderer.Clear(Color.DarkBlue);
+            
+            renderer.Render(rSprites);
+            renderer.Present();
+        }
+
+        private void Initialize(Arguments args)
+        {
+            renderer = new Glfw3Renderer();
+            if (!renderer.IsAvailable) throw new Exception("render not available");
+            IRenderDevice[] devs = renderer.GetDevices();
+            if (devs.Length < 1) throw new Exception("no devices found");
+            if (args.IsOptionPresent("s"))
+            {
+                SystemConsole.WriteLine("Available displays:");
+                for (int i=0; i< devs.Length; i++)
+                {
+                    SystemConsole.WriteLine($"\t<yellow>{i}: {devs[i].Name}<default>");
+                }
+                Environment.Exit(0);
+            }
+
+            SystemConsole.WriteLine("Initialize glfw3:");
+
+            if (args.IsOptionPresent("d"))
+            {
+                if (!int.TryParse(args.Options["d"].Value, out width)) throw new Exception("error parsing display number");
+                SystemConsole.WriteLine($"\t<yellow>Using {display}: {devs[display].Name}<default>");
+            }
+            renderer.AspectCorrection = ResizeMode.TouchFromOutside;
+            renderer.Closed += WindowClosed;
+            renderer.MouseButtonChanged += MBChanged;
+            renderer.Initialize(devs[display], rmode, RendererFlags.WaitRetrace, width, height, "OpenGL Test");
+            if (verbose) SystemConsole.WriteLine($"glfw3.Window: <yellow>{rmode}<default>, <green>{width}<default>x<green>{height}<default>");
+
+        }
+
+        private void MBChanged(object sender, glfw3.MouseButtonEventArgs e)
+        {
+            if (e.State == glfw3.InputState.Press)
+            {
+                SystemConsole.WriteLine($"mouse click: w[{e.Position.X}|{e.Position.Y}] s[{e.PositionNorm.X:F2}|{e.PositionNorm.Y:F2}]");
+
+            }
+
+            if (e.State == glfw3.InputState.Release)
+            {
+                if (e.PositionNorm.X < 0.1f && e.PositionNorm.Y < 0.1f) closed = true;
+
+                if (e.Button == glfw3.MouseButton.ButtonRight)
+                {
+                    renderer.AspectCorrection = renderer.AspectCorrection.Next();
+                }
+            }
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            closed = true;
+        }
+    }
+}
---
+++ OpenGLTest/TestImageGenerator.cs
@@ -0,0 +1,208 @@
+﻿#region CopyRight 2018
+/*
+    Copyright (c) 2019 Gernot Lenkner (andreas@rohleder.cc)
+    All rights reserved
+*/
+#endregion
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace OpenGLTest
+{
+    /// <summary>
+    /// static class that generates TestImages.
+    /// </summary>
+    public static class TestImageGenerator
+    {
+        /// <summary>
+        /// draws an ellipse on a bitmap.
+        /// </summary>
+        /// <param name="size">size of the returned bitmap in pixels</param>
+        /// <param name="foreground">color of the ellipse</param>
+        /// <param name="background">color of the background</param>
+        /// <param name="sizeratio">ratio of the size of the ellipse to the size of the bitmap. a ratio of 0.5 is default</param>
+        /// <param name="filled">if set the ellipse will be filled</param>
+        /// <returns><see cref="Bitmap"/></returns>
+        public static Bitmap GenerateEllipse(Size size, Color foreground, Color background, float sizeratio = 0.5f, bool filled = true)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Brush bg = new SolidBrush(background);
+                g.FillRectangle(bg, new Rectangle(Point.Empty, size));
+                RectangleF rec = new RectangleF((size.Width / 2) * (1 - sizeratio), (size.Height / 2) * (1 - sizeratio), (size.Width / 2) * (1 + sizeratio), (size.Height / 2) * (1 + sizeratio));
+                if (filled)
+                {
+                    Brush fg = new SolidBrush(foreground);
+                    g.FillEllipse(fg, rec.X - 0.5f, rec.Y - 0.5f, rec.Width, rec.Height);
+                }
+                else
+                {
+                    Pen fg = new Pen(foreground);
+                    g.DrawEllipse(fg, rec.X, rec.Y, rec.Width - 0.5f, rec.Height - 0.5f);
+                }
+            }
+            return bmp;
+        }
+
+        /// <summary>
+        /// draws a rectangle on a bitmap.
+        /// </summary>
+        /// <param name="size">size of the returned bitmap in pixels</param>
+        /// <param name="foreground">color of the rectangle</param>
+        /// <param name="background">color of the background</param>
+        /// <param name="sizeratio">ratio of the size of the rectangle to the size of the bitmap. a ratio of 0.5 is default</param>
+        /// <param name="filled">if set the rectangle will be filled</param>
+        /// <returns><see cref="Bitmap"/></returns>
+        public static Bitmap GenerateRectangle(Size size, Color foreground, Color background, float sizeratio = 0.5f, bool filled = true)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Brush bg = new SolidBrush(background);
+                g.FillRectangle(bg, new Rectangle(Point.Empty, size));
+                RectangleF rec = new RectangleF((size.Width / 2) * (1 - sizeratio), (size.Height / 2) * (1 - sizeratio), (size.Width) * (sizeratio), (size.Height) * (sizeratio));
+                if (filled)
+                {
+                    Brush fg = new SolidBrush(foreground);
+                    g.FillRectangle(fg, rec);
+                }
+                else
+                {
+                    Pen fg = new Pen(foreground);
+                    g.DrawRectangle(fg, rec.X, rec.Y, rec.Width - 0.5f, rec.Height - 0.5f);
+                }
+            }
+            return bmp;
+        }
+
+        /// <summary>
+        /// draws a text on a bitmap with FontFamily <see cref="FontFamily.GenericSansSerif"/>
+        /// </summary>
+        /// <param name="size">size of the returned bitmap in pixels</param>
+        /// <param name="foreground">color of the text</param>
+        /// <param name="background">color of the background</param>
+        /// <param name="text">the text</param>
+        /// <param name="sizeratio">ratio of the size of the rectangle to the size of the bitmap. a ratio of 0.5 is default</param>
+        /// <returns><see cref="Bitmap"/></returns>
+        public static Bitmap GenerateString(Size size, Color foreground, Color background, string text, float sizeratio = 0.5f)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Brush bg = new SolidBrush(background);
+                g.FillRectangle(bg, new Rectangle(Point.Empty, size));
+                RectangleF rec = new RectangleF((size.Width / 2) * (1 - sizeratio), (size.Height / 2) * (1 - sizeratio), (size.Width) * (sizeratio), (size.Height) * (sizeratio));
+                //Pen fgp = new Pen(foreground);
+                //g.DrawRectangle(fgp, rec.X, rec.Y, rec.Width - 0.5f, rec.Height - 0.5f);
+                Brush fgb = new SolidBrush(foreground);
+                g.DrawString(text, new Font(FontFamily.GenericSansSerif, rec.Height * 0.72f), fgb, rec);
+            }
+            return bmp;
+        }
+
+        /// <summary>
+        /// generates a bitmap with some default color stripes.
+        /// </summary>
+        /// <param name="size">the size of the bitmap in pixels</param>
+        /// <param name="horizontal">when set horizontal stripes will be generated</param>
+        /// <returns><see cref="Bitmap"/></returns>
+        public static Bitmap GenerateColorStripes(Size size, bool horizontal = false)
+        {
+            Color[] colortable = new Color[8]
+            {
+                Color.Red, Color.Green, Color.Blue, Color.White, Color.Magenta, Color.Cyan, Color.Yellow, Color.Black,
+            };
+            Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    Brush b = new SolidBrush(colortable[i]);
+                    PointF loc;
+                    SizeF rectsize;
+                    if (horizontal)
+                    {
+                        loc = new PointF(0, i * size.Height / 8f);
+                        rectsize = new SizeF(size.Width, size.Height / 8f);
+                    }
+                    else
+                    {
+                        loc = new PointF(i * size.Width / 8f, 0);
+                        rectsize = new SizeF(size.Width / 8f, size.Height);
+                    }
+                    RectangleF rect = new RectangleF(loc, rectsize);
+                    g.FillRectangle(b, rect);
+                }
+            }
+            return bmp;
+        }
+
+        /// <summary>
+        /// generates a bitmap with eight grey stripes in ascending brightness.
+        /// </summary>
+        /// <param name="size">the size of the bitmap in pixels</param>
+        /// <param name="horizontal">when set horizontal stripes will be generated</param>
+        /// <returns><see cref="Bitmap"/></returns>
+        public static Bitmap GenerateGrayStripes(Size size, bool horizontal = false)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    Brush b = new SolidBrush(Color.FromArgb(i * 32, i * 32, i * 32));
+                    PointF loc;
+                    SizeF rectsize;
+                    if (horizontal)
+                    {
+                        loc = new PointF(0, i * size.Height / 8f);
+                        rectsize = new SizeF(size.Width, size.Height / 8f);
+                    }
+                    else
+                    {
+                        loc = new PointF(i * size.Width / 8f, 0);
+                        rectsize = new SizeF(size.Width / 8f, size.Height);
+                    }
+                    RectangleF rect = new RectangleF(loc, rectsize);
+                    g.FillRectangle(b, rect);
+                }
+            }
+            return bmp;
+        }
+
+        /// <summary>
+        /// generates a bitmap with a checkerboard texture
+        /// </summary>
+        /// <param name="size">the size of the bitmap in pixels</param>
+        /// <param name="fieldsize">the size of one field in pixels</param>
+        /// <param name="foreground">the first field color</param>
+        /// <param name="background">the secon field color</param>
+        /// <returns><see cref="Bitmap"/></returns>
+        public static Bitmap GenerateCheckerBoard(Size size, Size fieldsize, Color foreground, Color background)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Brush[] colors = new Brush[2]
+                {
+               new SolidBrush(foreground),
+                new SolidBrush(background),
+                };
+                for (int y = 0; y < size.Height; y += fieldsize.Height)
+                {
+                    for (int x = 0; x < size.Width; x += fieldsize.Width)
+                    {
+                        Rectangle rect = new Rectangle(new Point(x, y), fieldsize);
+                        g.FillRectangle(colors[(y / fieldsize.Height + x / fieldsize.Width) % 2], rect);
+                    }
+                }
+            }
+            return bmp;
+        }
+
+    }
+
+}
