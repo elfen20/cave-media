6331cd5f65065812209a2d90464604b3f2836bc2
gernot <g.lenkner@outlook.de>
2019-04-10 17:58:47 +0200

    added test buttons

--- Bitmap32Test/Form1.Designer.cs
+++ Bitmap32Test/Form1.Designer.cs
@@ -29,7 +29,11 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.button1 = new System.Windows.Forms.Button();
+            this.bCopy = new System.Windows.Forms.Button();
+            this.bScale = new System.Windows.Forms.Button();
+            this.bRotate = new System.Windows.Forms.Button();
+            this.bFlipX = new System.Windows.Forms.Button();
+            this.bFlipY = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
@@ -39,28 +43,85 @@
             this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.splitContainer1.Location = new System.Drawing.Point(0, 37);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 71);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(6);
             this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Size = new System.Drawing.Size(615, 342);
-            this.splitContainer1.SplitterDistance = 305;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1230, 658);
+            this.splitContainer1.SplitterDistance = 610;
+            this.splitContainer1.SplitterWidth = 8;
             this.splitContainer1.TabIndex = 0;
             // 
-            // button1
+            // bCopy
             // 
-            this.button1.Location = new System.Drawing.Point(12, 8);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bCopy.Location = new System.Drawing.Point(24, 15);
+            this.bCopy.Margin = new System.Windows.Forms.Padding(6);
+            this.bCopy.Name = "bCopy";
+            this.bCopy.Size = new System.Drawing.Size(150, 44);
+            this.bCopy.TabIndex = 2;
+            this.bCopy.Text = "Copy";
+            this.bCopy.UseVisualStyleBackColor = true;
+            this.bCopy.Click += new System.EventHandler(this.bCopy_Click);
+            // 
+            // bScale
+            // 
+            this.bScale.Location = new System.Drawing.Point(186, 15);
+            this.bScale.Margin = new System.Windows.Forms.Padding(6);
+            this.bScale.Name = "bScale";
+            this.bScale.Size = new System.Drawing.Size(150, 44);
+            this.bScale.TabIndex = 3;
+            this.bScale.Text = "Scale";
+            this.bScale.UseVisualStyleBackColor = true;
+            this.bScale.Click += new System.EventHandler(this.bScale_Click);
+            // 
+            // bRotate
+            // 
+            this.bRotate.Location = new System.Drawing.Point(348, 15);
+            this.bRotate.Margin = new System.Windows.Forms.Padding(6);
+            this.bRotate.Name = "bRotate";
+            this.bRotate.Size = new System.Drawing.Size(150, 44);
+            this.bRotate.TabIndex = 4;
+            this.bRotate.Text = "Rotate";
+            this.bRotate.UseVisualStyleBackColor = true;
+            this.bRotate.Click += new System.EventHandler(this.bRotate_Click);
+            // 
+            // bFlipX
+            // 
+            this.bFlipX.Location = new System.Drawing.Point(510, 15);
+            this.bFlipX.Margin = new System.Windows.Forms.Padding(6);
+            this.bFlipX.Name = "bFlipX";
+            this.bFlipX.Size = new System.Drawing.Size(150, 44);
+            this.bFlipX.TabIndex = 5;
+            this.bFlipX.Text = "Flip X";
+            this.bFlipX.UseVisualStyleBackColor = true;
+            this.bFlipX.Click += new System.EventHandler(this.bFlipX_Click);
+            // 
+            // bFlipY
+            // 
+            this.bFlipY.Location = new System.Drawing.Point(672, 15);
+            this.bFlipY.Margin = new System.Windows.Forms.Padding(6);
+            this.bFlipY.Name = "bFlipY";
+            this.bFlipY.Size = new System.Drawing.Size(150, 44);
+            this.bFlipY.TabIndex = 6;
+            this.bFlipY.Text = "Flip Y";
+            this.bFlipY.UseVisualStyleBackColor = true;
+            this.bFlipY.Click += new System.EventHandler(this.bFlipY_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 381);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1230, 733);
+            this.Controls.Add(this.bFlipY);
+            this.Controls.Add(this.bFlipX);
+            this.Controls.Add(this.bRotate);
+            this.Controls.Add(this.bScale);
+            this.Controls.Add(this.bCopy);
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -72,7 +133,11 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bCopy;
+        private System.Windows.Forms.Button bScale;
+        private System.Windows.Forms.Button bRotate;
+        private System.Windows.Forms.Button bFlipX;
+        private System.Windows.Forms.Button bFlipY;
     }
 }
 
--- Bitmap32Test/Form1.cs
+++ Bitmap32Test/Form1.cs
@@ -1,20 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
+using Cave.Media;
 
 namespace Bitmap32Test
 {
     public partial class Form1 : Form
     {
+        Bitmap background;
+        Bitmap32 background32;
         public Form1()
         {
             InitializeComponent();
+            background = new Bitmap(100, 100);
+            using (Graphics g = Graphics.FromImage(background))
+            {
+                g.Clear(Color.Black);
+                g.FillRectangle(Brushes.Red, 50, 0, 50, 50);
+                g.FillRectangle(Brushes.Green, 0, 50, 50, 50);
+                g.FillRectangle(Brushes.Blue, 50, 50, 50, 50);
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                background.Save(ms, ImageFormat.Bmp);
+                background32 = Bitmap32.FromStream(ms);
+            }
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(background, 0, 0);
+        }
+
+        private void bCopy_Click(object sender, EventArgs e)
+        {
+            using (var g = splitContainer1.Panel2.CreateGraphics())
+            {
+                g.Clear(Color.Gray);
+                g.DrawImage(background32.ToGdiBitmap(), 0, 0);
+            }
+
+        }
+
+        private void bScale_Click(object sender, EventArgs e)
+        {
+            using (var g = splitContainer1.Panel2.CreateGraphics())
+            {
+                g.Clear(Color.Gray);
+                var b32 = background32.Resize(background32.Width * 2, background32.Height * 2, ResizeMode.None);
+                g.DrawImage(b32.ToGdiBitmap(), 0, 0);
+            }
+        }
+
+        private void bRotate_Click(object sender, EventArgs e)
+        {
+            using (var g = splitContainer1.Panel2.CreateGraphics())
+            {
+                g.Clear(Color.Gray);
+                var b32 = new Bitmap32(background32.Width, background32.Height);
+                b32.Draw(background32, 0, 0, new Translation() { Rotation = 1f });
+                g.DrawImage(b32.ToGdiBitmap(), 0, 0);
+            }
+        }
+
+        private void bFlipX_Click(object sender, EventArgs e)
+        {
+            using (var g = splitContainer1.Panel2.CreateGraphics())
+            {
+                g.Clear(Color.Gray);
+                var b32 = new Bitmap32(background32.Width, background32.Height);
+                b32.Draw(background32, 0, 0, new Translation() { FlipHorizontally = true });
+                g.DrawImage(b32.ToGdiBitmap(), 0, 0);
+            }
+        }
+
+        private void bFlipY_Click(object sender, EventArgs e)
+        {
+            using (var g = splitContainer1.Panel2.CreateGraphics())
+            {
+                g.Clear(Color.Gray);
+                var b32 = new Bitmap32(background32.Width, background32.Height);
+                b32.Draw(background32, 0, 0, new Translation() { FlipVertically = true });
+                g.DrawImage(b32.ToGdiBitmap(), 0, 0);
+            }
         }
     }
 }
